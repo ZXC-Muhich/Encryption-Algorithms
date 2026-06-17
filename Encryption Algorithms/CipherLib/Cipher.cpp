9293dfd1335d0127bbf4dae04a76b7ba7b4174fb
mykyta <mykyta.sidorov@kse.org.ua>
2026-06-17 04:19:19 +0300

    Added Cipher class hierarchy, Cipher.h and Cipher.cpp with Caesar and A1Z26 Cipher for extra point

---
+++ Encryption Algorithms/CipherLib/Cipher.cpp
@@ -0,0 +1,82 @@
+#include "Cipher.h"
+#include <cctype>
+#include <string>
+
+
+CaesarCipher::CaesarCipher(int key) : key_(key) {}
+
+std::string CaesarCipher::encrypt(const std::string& text) {
+    std::string result = "";
+
+    for (char c : text) {
+        if (isalpha(c)) {
+            char base = islower(c) ? 'a' : 'A';
+
+            int shift = (c - base + key_) % 26;
+            if (shift < 0) {
+                shift += 26;
+            }
+
+            result += (char)(base + shift);
+        }
+        else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+std::string CaesarCipher::decrypt(const std::string& text) {
+    CaesarCipher reverseCipher(-key_);
+    return reverseCipher.encrypt(text);
+}
+
+std::string A1Z26Cipher::encrypt(const std::string& text) {
+    std::string result = "";
+
+    for (size_t i = 0; i < text.length(); ++i) {
+        char c = text[i];
+
+        if (isalpha(c)) {
+            int letterNumber = (tolower(c) - 'a') + 1;
+            result += std::to_string(letterNumber);
+
+            if (i + 1 < text.length() && isalpha(text[i + 1])) {
+                result += "-";
+            }
+        }
+        else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+std::string A1Z26Cipher::decrypt(const std::string& text) {
+    std::string result = "";
+    std::string num = "";
+
+    for (char c : text) {
+        if (isdigit(c)) {
+            num += c;
+        }
+        else {
+            if (!num.empty()) {
+                int numberValue = std::stoi(num);
+                result += (char)('a' - 1 + numberValue);
+                num.clear();
+            }
+
+            if (c != '-') {
+                result += c;
+            }
+        }
+    }
+
+    if (!num.empty()) {
+        int numberValue = std::stoi(num);
+        result += (char)('a' - 1 + numberValue);
+    }
+
+    return result;
+}
--- Encryption Algorithms/CipherLib/Cipher.h
+++ Encryption Algorithms/CipherLib/Cipher.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <string>
+
+class Cipher {
+public:
+    virtual std::string encrypt(const std::string& text) = 0;
+    virtual std::string decrypt(const std::string& text) = 0;
+    virtual ~Cipher() = default;
+};
+
+class CaesarCipher : public Cipher {
+    int key_;
+public:
+    CaesarCipher(int key);
+    std::string encrypt(const std::string& text) override;
+    std::string decrypt(const std::string& text) override;
+};
+
+class VigenereCipher : public Cipher {
+    std::string key_;
+public:
+    VigenereCipher(const std::string& key);
+    std::string encrypt(const std::string& text) override;
+    std::string decrypt(const std::string& text) override;
+};
+
+class A1Z26Cipher : public Cipher {
+public:
+    A1Z26Cipher() = default;
+    std::string encrypt(const std::string& text) override;
+    std::string decrypt(const std::string& text) override;
+};
