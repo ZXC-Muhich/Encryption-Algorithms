9392704278aa7beaeb07c68d2dfe5ef465d16dbc
mykyta <mykyta.sidorov@kse.org.ua>
2026-06-25 09:16:16 +0300

    Forgot to add final edits, nothing new except "#define _CRT_SECURE_NO_WARNINGS"

--- Encryption Algorithms/CipherLib/cipher_api.cpp
+++ Encryption Algorithms/CipherLib/cipher_api.cpp
@@ -1,39 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "cipher_api.h"
 #include "Cipher.h"
 #include <cstring>
 
+char* allocate_c_string(const std::string& str) {
+    char* cstr = new char[str.length() + 1];
+    std::strcpy(cstr, str.c_str());
+    return cstr;
+}
+
 extern "C" {
 
     EXPORT cipher_t* cipher_create_caesar(int key) {
-        return new CaesarCipher(key);
+        return (cipher_t*)new CaesarCipher(key);
     }
 
     EXPORT cipher_t* cipher_create_vigenere(const char* key) {
-        return new VigenereCipher(std::string(key));
+        return (cipher_t*)new VigenereCipher(std::string(key));
     }
 
     EXPORT cipher_t* cipher_create_a1z26() {
-        return new A1Z26Cipher();
+        return (cipher_t*)new A1Z26Cipher();
     }
-
-
-    char* allocate_c_string(const std::string& str) {
-        char* cstr = new char[str.length() + 1];
-        std::strcpy(cstr, str.c_str());
-        return cstr;
-    }
-
 
     EXPORT char* cipher_encrypt(cipher_t* cipher, const char* text) {
-        return allocate_c_string(static_cast<Cipher*>(cipher)->encrypt(text));
+
+        return allocate_c_string(((Cipher*)cipher)->encrypt(text));
     }
 
     EXPORT char* cipher_decrypt(cipher_t* cipher, const char* text) {
-        return allocate_c_string(static_cast<Cipher*>(cipher)->decrypt(text));
+        return allocate_c_string(((Cipher*)cipher)->decrypt(text));
     }
 
     EXPORT void cipher_destroy(cipher_t* cipher) {
-        delete static_cast<Cipher*>(cipher);
+        delete (Cipher*)cipher;
     }
 
     EXPORT void cipher_free(char* str) {
